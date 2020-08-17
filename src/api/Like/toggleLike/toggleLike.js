2d1d36555bb5dcc89a377abcb2a8edd4b24a7352
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-08-17 23:09:01 +0900

    백엔드 구성..
    *일단 백엔드 구성후...프론트엔드 공부후에 다시 복습하자

---
+++ src/api/Like/toggleLike/toggleLike.js
@@ -0,0 +1,50 @@
+import { isAuthenticated } from "../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    toggleLike: async (_, args, { request }) => {
+      isAuthenticated(request);
+      const { postId } = args;
+      const { user } = request;
+
+      const fliterOptions = {
+        AND: [
+          {
+            user: {
+              id: user.id,
+            },
+          },
+          {
+            post: {
+              id: postId,
+            },
+          },
+        ],
+      };
+      try {
+        const existingLike = await prisma.$exists.like(fliterOptions);
+
+        if (existingLike) {
+          await prisma.deleteManyLikes(fliterOptions);
+        } else {
+          await prisma.createLike({
+            user: {
+              connect: {
+                id: user.id,
+              },
+            },
+            post: {
+              connect: {
+                id: postId,
+              },
+            },
+          });
+        }
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
 import "./env";
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
-
 import schema from "./schema";
 import "./passport";
 import { authenticateJwt } from "./passport";
-import { prisma } from "../generated/prisma-client";
+import { isAuthenticated } from "./middlewares";
 
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({
   schema,
-  context: ({ request }) => ({ request }),
+  context: ({ request }) => ({ request, isAuthenticated }),
 });
 
 server.express.use(logger("dev"));
