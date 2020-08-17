10e30f95be7414b5cb25c6a345f348d98ee58587
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-08-18 02:10:43 +0900

    프래그먼트 마무리

--- src/fragments.js
+++ src/fragments.js
@@ -1,36 +1,33 @@
-// export const USER_FRAGMENT = `
-//    id
-//    name
-// `;
-
+export const USER_FRAGMENT = `
+  id
+  name
+`;
 export const COMMENT_FRAGMENT = `
-fragment CommentParts on Comment{
   id
   text
   user{
-    name
-}
+    ${USER_FRAGMENT}
 }
 `;
 
-// export const FILE_FRAGMENT = `
-//   id
-//   url
-// `;
+export const FILE_FRAGMENT = `
+  id
+  url
+`;
 
-// export const FULL_POST_FRAGMENT = `
-// fragment PostParts on Post{
-//   id
-//   location
-//   caption
-//   files{
-//     ${FILE_FRAGMENT}
-//   }
-//   comments{
-//    ${COMMENT_FRAGMENT}
-//   }
-//   user{
-//     ${USER_FRAGMENT}
-//   }
-// }
-// `;
+export const FULL_POST_FRAGMENT = `
+fragment PostParts on Post{
+    id
+    location
+    caption
+  files{
+    ${FILE_FRAGMENT}
+  }
+  comments{
+    ${COMMENT_FRAGMENT}
+  }
+  user{
+    ${USER_FRAGMENT}
+  }
+}
+`;
